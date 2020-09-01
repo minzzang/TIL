cc3fd1c215aafe7ce6c55d60ccc8f4cc92703567
minzzang <minjjang1117@gmail.com>
2020-09-01 22:37:31 +0900

    dfs

--- 알고리즘/DFSBFS/pro_타겟넘버.java
+++ 알고리즘/DFSBFS/pro_타겟넘버.java
@@ -2,17 +2,22 @@
   * About
   *
   * Author: minzzang@GitHub (minjjang1117@gmail.com)
-  * Date  : 2020-08-31
+  * Date  : 2020-09-01
   * URL   : https://programmers.co.kr/learn/courses/30/lessons/43165
   *
   */
 
 class Solution {
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        return answer;
+        return dfs(numbers, target, 0, 0);
     }
-
-    private static void dfs(int[] numbers, int target) {
+    
+    private static int dfs(int[] numbers, int target, int sum, int index) {
+        if (index == numbers.length) {
+            return sum == target ? 1 : 0;
+        } else {
+            return dfs(numbers, target, sum+numbers[index], index+1)
+                    + dfs(numbers, target, sum-numbers[index], index+1);
+        }
     }
 }
