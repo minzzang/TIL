f0c99c7b5dca2db9b98fbe039d5345cdd6803840
minzzang <minjjang1117@gmail.com>
2020-09-02 22:38:36 +0900

    dfs

---
+++ 알고리즘/DFSBFS/pro_네트워크.java
@@ -0,0 +1,15 @@
+ /*
+  * About
+  *
+  * Author: minzzang@GitHub (minjjang1117@gmail.com)
+  * Date  : 2020-09-02
+  * URL   : https://programmers.co.kr/learn/courses/30/lessons/43162
+  *
+  */
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        return answer;
+    }
+}
