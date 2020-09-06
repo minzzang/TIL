4a13cb06c430cf25d8712ea26aab6869f6870fdb
minzzang <minjjang1117@gmail.com>
2020-09-06 23:58:42 +0900

    dfs

--- 알고리즘/DFSBFS/pro_네트워크.java
+++ 알고리즘/DFSBFS/pro_네트워크.java
@@ -2,19 +2,25 @@
   * About
   *
   * Author: minzzang@GitHub (minjjang1117@gmail.com)
-  * Date  : 2020-09-05
+  * Date  : 2020-09-06
   * URL   : https://programmers.co.kr/learn/courses/30/lessons/43162
   *
   */
 
 class Solution {
+
+    private static boolean[] d;
+
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        dfs(n, computers);
+        dfs(computers, 0);
         return answer;
     }
 
-    private static void dfs(int n, int[][] computers) {
+    private static void dfs(int[][] computers, int index) {
+         if (d[index] == false) {
+            d[index] = true;
+         }
 
     }
 }
