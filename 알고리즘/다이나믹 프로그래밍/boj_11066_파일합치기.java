6597c4e5a8938f4bb8454e66763667c2f8d515b6
minzzang <minjjang1117@gmail.com>
2020-11-08 22:43:47 +0900

    1108

--- 알고리즘/다이나믹 프로그래밍/boj_11066_파일합치기.java
+++ 알고리즘/다이나믹 프로그래밍/boj_11066_파일합치기.java
@@ -20,15 +20,32 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
+        StringBuffer sb = new StringBuffer();
         while (T-- > 0) {
             int k = Integer.parseInt(br.readLine());
-            int[] arr = new int[k];
+            int[] arr = new int[k+1];
+            int[] sum = new int[k+1];
+            int[][] dp = new int[k+1][k+1];
 
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < k; i++) {
+            for (int i = 1; i <= k; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
+                sum[i] = sum[i-1] + arr[i];
             }
-        }
 
+            for (int d = 1; d <= k; d++) {
+                for (int y = 1; y + d <= k; y++) {
+                    int x = y + d;
+                    dp[y][x] = Integer.MAX_VALUE;
+
+                    for (int mid = y; mid < x; mid++) {
+                        dp[y][x] =
+                                Math.min(dp[y][x], dp[y][mid] + dp[mid + 1][x] + sum[x] - sum[y - 1]);
+                    }
+                }
+            }
+            sb.append(dp[1][k]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
