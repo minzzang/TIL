5a99519ba8c2d227507d1faa02d5066c76a84c09
minzzang <minjjang1117@gmail.com>
2020-12-24 23:59:20 +0900

    1224

---
+++ 알고리즘/lv3/pro_셔틀버스.java
@@ -0,0 +1,15 @@
+ /*
+  * About
+  *
+  * Author: minzzang@GitHub (minjjang1117@gmail.com)
+  * Date  : 2020-12-24
+  * URL   : https://programmers.co.kr/learn/courses/30/lessons/17678
+  *
+  */
+
+class Solution {
+    public String solution(int n, int t, int m, String[] timetable) {
+        String answer = "";
+        return answer;
+    }
+}
