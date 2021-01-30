066c6985b57ed5236800fb43ed0a594db85ab477
minzzang <minjjang1117@gmail.com>
2021-01-30 23:59:28 +0900

    0130

---
+++ 알고리즘/완전탐색/boj_1107_리모컨.java
@@ -0,0 +1,30 @@
+ /*
+  * About
+  *
+  * Author: minzzang@GitHub (minjjang1117@gmail.com)
+  * Date  : 2021-01-29
+  * URL   : https://www.acmicpc.net/problem/13460
+  *
+  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        char[] ch = new char[N][M];
+        for (int i=0; i<N; i++) {
+	for (int j = 0; j < M; j++) {
+	
+	}
+        }
+    }
+}
