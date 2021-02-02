eb71d43042facc1b6d7ea0949cd701b8f50ffeee
minzzang <minjjang1117@gmail.com>
2021-02-02 23:58:36 +0900

    0202

---
+++ 알고리즘/leetcode/Number of 1 Bits.java
@@ -0,0 +1,15 @@
+ /*
+  * About
+  *
+  * Author: minzzang@GitHub (minjjang1117@gmail.com)
+  * Date  : 2021-02-02
+  * URL   : https://leetcode.com/problems/number-of-1-bits
+  *
+  */
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        
+    }
+}
