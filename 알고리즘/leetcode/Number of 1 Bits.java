1fb4af1bbb22a36b60571fcab9885ff8b2bb6b00
minzzang <minjjang1117@gmail.com>
2021-02-03 23:31:59 +0900

    0203

--- 알고리즘/leetcode/Number of 1 Bits.java
+++ 알고리즘/leetcode/Number of 1 Bits.java
@@ -8,8 +8,16 @@
   */
 
 public class Solution {
-    // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        
+        int bits = 0;
+        int mask = 1;
+
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                bits++;
+            }
+            mask = mask << 1;
+        }
+        return bits;
     }
 }
