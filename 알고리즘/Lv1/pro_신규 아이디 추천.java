bad3ee2ea26b45bd05cac8703b2ca91f00e6d674
minzzang <minjjang1117@gmail.com>
2021-02-05 23:59:27 +0900

    0205

---
+++ 알고리즘/Lv1/pro_신규 아이디 추천.java
@@ -0,0 +1,17 @@
+ /*
+  * About
+  *
+  * Author: minzzang@GitHub (minjjang1117@gmail.com)
+  * Date  : 2021-02-05
+  * URL   : https://programmers.co.kr/learn/courses/30/lessons/72410
+  *
+  */
+
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        String lowerId = new_id.toLowerCase();
+        
+        return answer;
+    }
+}
