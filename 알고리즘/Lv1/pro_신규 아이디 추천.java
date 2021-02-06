62ff407f4597cb2532db294ff2f07bdd579eeee3
minzzang <minjjang1117@gmail.com>
2021-02-06 23:59:38 +0900

    0206

--- 알고리즘/Lv1/pro_신규 아이디 추천.java
+++ 알고리즘/Lv1/pro_신규 아이디 추천.java
@@ -11,7 +11,41 @@ class Solution {
     public String solution(String new_id) {
         String answer = "";
         String lowerId = new_id.toLowerCase();
+        String match = "[^0-9a-zA-Z-_.]";
+        lowerId = lowerId.replaceAll(match, "");
         
-        return answer;
+        match = "[.]{2,}";
+        lowerId = lowerId.replaceAll(match, ".");
+        
+        if (!lowerId.isEmpty()) {
+            if (lowerId.charAt(0) == '.') {
+                lowerId = lowerId.substring(1);
+            }
+        } 
+        if (!lowerId.isEmpty()) {
+            if (lowerId.charAt(lowerId.length()-1) == '.') {
+                lowerId = lowerId.substring(0, lowerId.length()-1);
+            }
+        } 
+        
+        if (lowerId.isEmpty()) lowerId += "a";
+        
+        if (lowerId.length() >= 16) {
+            lowerId = lowerId.substring(0, 15);
+            if (lowerId.charAt(lowerId.length() - 1) == '.') {
+                lowerId = lowerId.substring(0, 14);
+            }
+        }
+        
+        if (lowerId.length() <= 2) {
+            if (lowerId.length() == 1) {
+                lowerId += lowerId + lowerId;
+            } else {
+                String last = lowerId.charAt(1) + "";
+                lowerId += last;
+            }
+        }
+        
+        return lowerId;
     }
 }
