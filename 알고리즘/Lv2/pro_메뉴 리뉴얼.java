a2317002d53f92054b2ce6faea5c6440c21fed59
minzzang <minjjang1117@gmail.com>
2021-02-07 23:59:00 +0900

    0207

---
+++ 알고리즘/Lv2/pro_메뉴 리뉴얼.java
@@ -0,0 +1,15 @@
+ /*
+  * About
+  *
+  * Author: minzzang@GitHub (minjjang1117@gmail.com)
+  * Date  : 2021-02-07
+  * URL   : https://programmers.co.kr/learn/courses/30/lessons/72411
+  *
+  */
+
+class Solution {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        return answer;
+    }
+}
