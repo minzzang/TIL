0eb42cb528bbc84ef5e0d052237c7c67bc1d86e1
minzzang <minjjang1117@gmail.com>
2021-02-08 23:59:39 +0900

    0208

---
+++ 알고리즘/Lv2/pro_순위 검색.java
@@ -0,0 +1,15 @@
+ /*
+  * About
+  *
+  * Author: minzzang@GitHub (minjjang1117@gmail.com)
+  * Date  : 2021-02-08
+  * URL   : https://programmers.co.kr/learn/courses/30/lessons/72412
+  *
+  */
+
+class Solution {
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = {};
+        return answer;
+    }
+}
