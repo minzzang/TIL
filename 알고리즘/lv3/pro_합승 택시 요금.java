39ddff93f915c70669f11a47da76000f2348d5ca
minzzang <minjjang1117@gmail.com>
2021-02-09 23:59:00 +0900

    0209

---
+++ 알고리즘/lv3/pro_합승 택시 요금.java
@@ -0,0 +1,15 @@
+ /*
+  * About
+  *
+  * Author: minzzang@GitHub (minjjang1117@gmail.com)
+  * Date  : 2021-02-09
+  * URL   : https://programmers.co.kr/learn/courses/30/lessons/72413
+  *
+  */
+
+class Solution {
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = 0;
+        return answer;
+    }
+}
