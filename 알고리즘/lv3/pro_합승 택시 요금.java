7edb72d484fad93b43437cb0092f10e805543277
Min Je Jang <minjjang1117@gmail.com>
2021-02-12 23:57:02 +0900

    Update pro_합승 택시 요금.java

--- 알고리즘/lv3/pro_합승 택시 요금.java
+++ 알고리즘/lv3/pro_합승 택시 요금.java
@@ -2,7 +2,7 @@
   * About
   *
   * Author: minzzang@GitHub (minjjang1117@gmail.com)
-  * Date  : 2021-02-11
+  * Date  : 2021-02-12
   * URL   : https://programmers.co.kr/learn/courses/30/lessons/72413
   *
   */
