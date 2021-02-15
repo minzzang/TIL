7b148a24be2dc73a934a8de0b17c59e3445316d3
minzzang <minjjang1117@gmail.com>
2021-02-15 23:56:45 +0900

    0215

--- 알고리즘/Lv2/pro_순위 검색.java
+++ 알고리즘/Lv2/pro_순위 검색.java
@@ -2,7 +2,7 @@
   * About
   *
   * Author: minzzang@GitHub (minjjang1117@gmail.com)
-  * Date  : 2021-02-08
+  * Date  : 2021-02-15
   * URL   : https://programmers.co.kr/learn/courses/30/lessons/72412
   *
   */
@@ -10,6 +10,15 @@
 class Solution {
     public int[] solution(String[] info, String[] query) {
         int[] answer = {};
+
+        for (String one : query) {
+            String[] ones = one.split("and ");
+            String food = ones[3].split(" ")[0];
+            String score = ones[3].split(" ")[1];
+
+            Arrays.stream(info)
+                    .filter(s -> s.contains(food));
+        }
         return answer;
     }
 }
