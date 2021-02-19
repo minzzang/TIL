c8af8deb44808dccb9a83d318f7feca5c8217655
Min Je Jang <minjjang1117@gmail.com>
2021-02-19 23:53:43 +0900

    Update pro_순위 검색.java

--- 알고리즘/Lv2/pro_순위 검색.java
+++ 알고리즘/Lv2/pro_순위 검색.java
@@ -2,7 +2,7 @@
   * About
   *
   * Author: minzzang@GitHub (minjjang1117@gmail.com)
-  * Date  : 2021-02-18
+  * Date  : 2021-02-19
   * URL   : https://programmers.co.kr/learn/courses/30/lessons/72412
   *
   */
