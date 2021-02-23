9fe1e103db4f67c8c9ed52c0ab6255f8d1e6f056
Min Je Jang <minjjang1117@gmail.com>
2021-02-23 23:52:13 +0900

    Update Sum of Nodes with Even-Valued Grandparent.java

--- 알고리즘/leetcode/Sum of Nodes with Even-Valued Grandparent.java
+++ 알고리즘/leetcode/Sum of Nodes with Even-Valued Grandparent.java
@@ -2,7 +2,7 @@
   * About
   *
   * Author: minzzang@GitHub (minjjang1117@gmail.com)
-  * Date  : 2021-02-20
+  * Date  : 2021-02-23
   * URL   : https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
   *
   */
